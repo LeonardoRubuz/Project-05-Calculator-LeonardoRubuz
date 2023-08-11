983dba0291135dcb767c83f9101abdaad7aeb039
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-08-11 08:03:31 +0100

    personal script file created

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 
 // TODO: Faire la manipulation du DOM dans ce fichier
 // First commit
+document.getElementById('calcul').innerHTML = "Leonardo";
---
+++ test.js
@@ -0,0 +1,1 @@
+document.getElementById('calcul').innerHTML = "Leonardo";
