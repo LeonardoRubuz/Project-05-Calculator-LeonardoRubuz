9abddd01a2c84ef9540acb25e68b1c6c0567df3a
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-08-11 10:07:10 +0100

    addDigit() added to the click event for  digits

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ const upperLabel = document.getElementById('calcul');
 
 const digits = document.getElementsByClassName('digit')
 //console.log(digits);
+
+// Fonctions d'évènements
+function addDigit() {
+    bottomInput.value += this.innerText;
+}
+
+// Ajout des évènements
+for (let index = 0; index < digits.length; index++) {
+    const element = digits[index];
+    element.addEventListener('click', addDigit)
+}
