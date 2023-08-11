33ee5b44b6bbc9bb9c79f0f6cd065a9a1203c072
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-08-11 11:04:55 +0100

    addToLabel() added. Corrections to do

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // TODO: Faire la manipulation du DOM dans ce fichier
 
-// Selection des éléments
+// SELECTION DES ELEMENTS
 const bottomInput = document.getElementById('input');
 const upperLabel = document.getElementById('calcul');
 
@@ -10,17 +10,26 @@ const digits = document.getElementsByClassName('digit')
 const operators = document.querySelectorAll('#minus, #times, #divideby, #plus');
 //console.log(operators);
 
-// Fonctions d'évènements
+// FONCTIONS D'EVENEMENTS
 function addDigit() {
     bottomInput.value += this.innerText;
 }
 
 function addToLabel() {
-    upperLabel.innerText += bottomInput.value + ' '+ this.innerText + '';
+    upperLabel.innerText += bottomInput.value + ' '+ this.innerText + ' ';
+    bottomInput.value = "";
+    bottomInput.name = "";
 }
 
-// Ajout des évènements
+// AJOUT DES EVENEMENTS
+// sur les bouttons des nombres
 for (let index = 0; index < digits.length; index++) {
     const element = digits[index];
     element.addEventListener('click', addDigit)
+}
+//sur les opérateurs
+for (let i = 0; i < operators.length; i++) {
+    const element = operators[i];
+    element.addEventListener('click', addToLabel)
+    
 }
